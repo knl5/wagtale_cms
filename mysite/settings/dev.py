161d8f6aeafee236a71eec525d2cf948e0dfa955
Kanelle Lorent <kanelle.lorent@mail-ecv.fr>
2022-06-09 16:26:47 +0200

    first commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&(-!x4@&4_d4j1a#q=^^46@!!=a1tgb@_yhxkmmybh5lu8ptd)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
